5f74d89bbe7804af27c9c665450ee220465aa80d
gengauss <shiroemonmadeon@gmail.com>
2020-09-08 23:36:47 +0900

    add: healthbook models

--- apps/healthbook/models/mental_info.py
+++ apps/healthbook/models/mental_info.py
@@ -6,3 +6,15 @@ class MentalInfo(models.Model):
     class Meta:
         db_table = 'mental_info'
         get_latest_by = 'created'
+
+    general_information = models.TextField(default=None)
+    symptoms = models.TextField(default=None)
+    prevention = models.TextField(default=None)
+    treatment = models.TextField(default=None)
+
+
+class MentalInfoAdmin(admin.ModelAdmin):
+    list_display = ('general_information', 'symptoms', 'prevention', 'treatment')
+
+
+admin.site.register(MentalInfo, MentalInfoAdmin)
--- apps/healthbook/models/physical_info.py
+++ apps/healthbook/models/physical_info.py
@@ -6,3 +6,15 @@ class PhysicalInfo(models.Model):
     class Meta:
         db_table = 'physical_info'
         get_latest_by = 'created'
+
+    general_information = models.TextField(default=None)
+    symptoms = models.TextField(default=None)
+    prevention = models.TextField(default=None)
+    treatment = models.TextField(default=None)
+
+
+class PhysicalInfoAdmin(admin.ModelAdmin):
+    list_display = ('general_information', 'symptoms', 'prevention', 'treatment')
+
+
+admin.site.register(PhysicalInfo, PhysicalInfoAdmin)
