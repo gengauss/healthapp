48eae891f57d7aafe3a79708d7e350cb37aa2681
gengauss <shiroemonmadeon@gmail.com>
2020-09-18 00:39:07 +0900

    done basic healthbook

--- apps/healthbook/urls.py
+++ apps/healthbook/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', healthbook.index, name="healthbook"),
     path('physicalhealth', healthbook.physical_health, name='physicalhealth'),
     path('mentalhealth', healthbook.mental_health, name='mentalhealth'),
+    path('mentalhealth/<int:mentalinfo_id>', healthbook.mental_detail, name='mentaldetail')
 ]
--- apps/healthbook/views/healthbook.py
+++ apps/healthbook/views/healthbook.py
@@ -1,5 +1,5 @@
-from django.urls import path
 from django.shortcuts import render
+from ..models import MentalInfo
 
 
 def index(request):
@@ -11,4 +11,12 @@ def physical_health(request):
 
 
 def mental_health(request):
-    return render(request, '../templates/healthbook/mentalhealth.html', {})
+    mentaldiseases = MentalInfo.objects.all()
+    data_dict = {'mentaldiseases': mentaldiseases}
+    return render(request, '../templates/healthbook/mentalhealth.html', data_dict)
+
+
+def mental_detail(request, mentalinfo_id):
+    mentaldiseases = MentalInfo.objects.get(pk=mentalinfo_id)
+    data_dict = {'mentaldiseases': mentaldiseases}
+    return render(request, '../templates/healthbook/detail.html', data_dict)
