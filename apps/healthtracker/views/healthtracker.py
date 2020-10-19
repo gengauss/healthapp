79025201393983c80e229e91fc70e02ef72a2038
gengauss <shiroemonmadeon@gmail.com>
2020-10-19 23:05:18 +0900

    login, logout

--- apps/healthtracker/views/healthtracker.py
+++ apps/healthtracker/views/healthtracker.py
@@ -15,7 +15,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('healthtracker')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, '../templates/healthtracker/register.html', {'form': form})
