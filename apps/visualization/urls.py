f182bfce74ca9397c822bd42b3cd27d82f2c4e29
gengauss <shiroemonmadeon@gmail.com>
2020-10-24 21:45:55 +0900

    add visualization for diabetes

--- apps/visualization/urls.py
+++ apps/visualization/urls.py
@@ -4,4 +4,5 @@ from .views import visualization
 urlpatterns = [
     path('', visualization.index, name="visualization"),
     path('surveyresult', visualization.survey_result, name='surveyresult'),
+    path('diabetes', visualization.diabetes_result, name='diabetes'),
 ]
--- apps/visualization/views/visualization.py
+++ apps/visualization/views/visualization.py
@@ -1,9 +1,11 @@
+from bokeh.embed import components
+from bokeh.plotting import figure
 from django.shortcuts import render
 
 from .survey import plot_country, plot_age, plot_occupation, plot_gender, plot_water, plot_diet, plot_meals, \
     plot_meals, plot_first_meal, plot_last_meal, plot_same_meal
-from .tools import plot_bar_chart
-from ..models import Survey
+from .tools import plot_bar_chart, plot_count
+from ..models import Survey, Diabetes
 from django.db import connection
 import pandas as pd
 
@@ -31,7 +33,8 @@ def survey_result(request):
     mental_important = {'important': important_cols, 'counts': important_counts}
     mental_important_df = pd.DataFrame(mental_important)
     script_mental_important, div_mental_important = plot_bar_chart(mental_important_df['important'],
-                                                                   mental_important_df['counts'])
+                                                                   mental_important_df['counts'],
+                                                                   title="Mental Health Importance")
 
     data_dict = {'survey': survey, 'script_country': script_country, 'div_country': div_country,
                  'script_age': script_age, 'div_age': div_age,
@@ -45,3 +48,23 @@ def survey_result(request):
                  'script_same_meal': script_same_meal, 'div_same_meal': div_same_meal,
                  'script_mental_important': script_mental_important, 'div_mental_important': div_mental_important}
     return render(request, '../templates/visualization/survey.html', data_dict)
+
+
+def diabetes_result(request):
+    query = str(Diabetes.objects.all().query)
+    diabetes = pd.read_sql_query(query, connection)
+
+    # tested
+    tested_cols = diabetes['tested'].value_counts().keys().tolist()
+    tested_counts = diabetes['tested'].value_counts().tolist()
+    tested = {'tested': tested_cols, 'counts': tested_counts}
+    tested_df = pd.DataFrame(tested)
+    script_tested, div_tested = plot_count(tested_df['tested'], tested_df['counts'], "Tested Counts")
+
+    # age
+    age = diabetes['age'].value_counts().tolist()
+    script_age, div_age = plot_count(tested_cols, age, "Age")
+
+    data_dict = {'script_tested': script_tested, 'div_tested': div_tested,
+                 'script_age': script_age, 'div_age': div_age}
+    return render(request, '../templates/visualization/diabetes.html', data_dict)
